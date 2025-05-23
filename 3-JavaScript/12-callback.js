273f3946278186771cacb40ed5e0ba717ea372fb
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-05-23 22:03:14 +0530

    callback_functions

---
+++ 3-JavaScript/12-callback.js
@@ -0,0 +1,64 @@
+
+// function buildhouse(afterbuilding){
+// console.log("building the house")
+// afterbuilding();
+// }
+
+// function takephoto(){
+//     console.log("taking photo");
+// }
+
+// buildhouse(takephoto);
+
+
+// function arrivalbus(depature){
+//     console.log("the bus has arrived get in")
+//     depature();
+// }
+
+// function resting(){
+//     console.log("bus has stopped you can use restrooms if needed");
+    
+// }
+
+// arrivalbus(resting);
+
+
+// function coffeshop(cofferrived){
+//     console.log("orderd the coffe and recived after 5 min");
+// cofferrived();
+// }
+// function bill(){
+//     console.log("paid the bill and went home");
+// }
+// coffeshop(bill);
+
+
+
+
+// function foodorder(payfororder){
+//     console.log("orderd the food from Zomato")
+//     payfororder();
+// }
+
+// function recivedorder(){
+//     console.log("recived the order and its so fresh")
+// }
+
+// foodorder(recivedorder);
+
+
+
+function buildcar(sellcar){
+    console.log("we are building a new car this year")
+    sellcar();
+}
+
+function sellcar(){
+    console.log("and we are quoted our car for 8 lakhs")
+
+}
+buildcar(sellcar);
+
+
+
