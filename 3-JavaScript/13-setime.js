9f5f909a0df568a71e49de6edc2e7a4edd87aab1
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-05-24 19:32:38 +0530

    settimeout,setInterval

---
+++ 3-JavaScript/13-setime.js
@@ -0,0 +1,91 @@
+// function clotheslist(){
+//     console.log("There are multi-branded clothes in this list")
+// }
+
+// clotheslist();
+
+//---------------------------------
+
+//Arrow functions
+
+// let phone =()=>{
+//     console.log("hello")
+
+// }
+// phone();
+
+// //----
+// let greetguests=(greet) =>{
+// console.log(greet+ "welcome to our home");
+// }
+// greetguests("namasthey ");
+
+//--
+
+// let pg=(pgname)=>{
+//     console.log('my pg name is '+pgname);
+// }
+
+// pg("Stay cool luxury suites")
+
+// //----
+
+// let book=(bookname)=>{
+// let writtenby=(authorname)=>
+//     console.log("my fav book is "+ bookname + "the author name is " +authorname);
+// writtenby("edwin")
+// }
+// book("man and the sea ");
+//--------------------------------------------------------------------------
+
+//now we are doing template literals
+
+// let name= "venkatesh";
+// let age = "25";
+// let message= `my name is ${name}and my age is ${age} years old.`;
+// console.log(message);
+
+
+
+// let phone ="oneplus";
+// let cost ="30,0000";
+
+// let orderdmobile=`i orderd ${phone} and it costs me around ${cost}`;
+// console.log(orderdmobile);
+
+
+// //setTimeout
+
+// settimeout(())
+
+
+// setTimeout(()=>{
+//     console.log("hello");
+
+// }, 5000);
+
+// let greet=()=>{
+//     console.log("timing functions");
+// }
+// setTimeout(greet,3000); 
+
+// let alarm =()=>{
+//     console.log ("Wake me up at 5 am ");
+// }
+
+// setTimeout(alarm,8000);
+
+
+// let flights=()=>{
+//     console.log("Your flight is delayed for 2 hours")
+// }
+
+// setTimeout(flights,5000);
+
+//now we are learning about setInterval
+  
+// let greet=()=>{
+//     console.log('namasthey welcome our home')
+// }
+// setInterval(greet,3000);
+
