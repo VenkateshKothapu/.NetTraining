597ca0d01ec817625ae64987f06a18cc1b71b5ee
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-05-26 08:32:05 +0530

    debugging23052025

---
+++ 3-JavaScript/16-Debugging.js
@@ -0,0 +1,59 @@
+// function sayHello(name) {
+//     return "Hello" + Name;
+// }
+
+// let greet = sayHello("Venkatesh");
+// console.log(greet);
+
+
+// let fooditem="pizza"
+// document.write("i like to have "+fooditem);
+// debugger;
+
+// let bikes="sports bikes";
+// document.write("i like "+bikes);
+//debugger
+
+
+
+// Inspector - developer
+// Car       - code
+// Highway  - 
+// stop point - debugger;
+
+let foodItemName = "Pizza";
+//console.log(foodItemName);
+document.write(foodItemName);
+//debugger;  //stop point
+
+let deliveryLocation = "New York City";
+//console.log(deliveryLocation);
+document.write(deliveryLocation);
+//debugger;  //stop point
+
+
+let estimateDeliveryTime = "20 Mins";
+//console.log(estimateDeliveryTime);
+document.write(estimateDeliveryTime);
+//debugger;  //stop point
+
+
+let isDeliveryAvailable = true;
+//console.log(isDeliveryAvailable);
+document.write(isDeliveryAvailable);
+//debugger;  //stop point
+
+
+function bikes(){
+    document.write("i like RE");
+}
+bikes();
+
+
+function cars(){
+    bikes();
+    
+    document.write("i like BMW");
+}
+cars();
+
