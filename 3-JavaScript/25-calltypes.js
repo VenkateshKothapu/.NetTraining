a8ae9d3cbc378a42c2ccbc25d1cb74a3ce2a3f74
venkatesh <venkateshreddy.fullstack@gmail.com>
2025-06-10 01:03:07 +0530

    asynchronous and synchronus calls

---
+++ 3-JavaScript/25-calltypes.js
@@ -0,0 +1,17 @@
+
+//example of synchronous call-
+
+console.log("wake up");
+console.log("fresh up");
+console.log("have breakfast");
+console.log("gp to work");
+
+
+//example of asynchronous call-
+
+console.log("hey how are you")
+setTimeout(function(){
+    console.log("ok lets go for a coffe")
+},3000);
+
+console.log("long time no see");
